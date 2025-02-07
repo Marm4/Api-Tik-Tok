4d09b8896e293aea7d4e33f751350482ebc93193
marm4 <marcomasciulli96@gmail.com>
2025-02-07 15:32:00 -0300

    Change from mp3 to m4a

--- app.py
+++ app.py
@@ -16,24 +16,24 @@ def download_audio():
         return jsonify({"error": "Falta la URL"}), 400
 
     try:
-        # Opciones para descargar solo el audio
+        # Opciones para descargar el audio en formato m4a
         ydl_opts = {
             'format': 'bestaudio/best',
             'outtmpl': os.path.join(DOWNLOAD_FOLDER, '%(title)s.%(ext)s'),
             'postprocessors': [{
                 'key': 'FFmpegExtractAudio',
-                'preferredcodec': 'mp3',
+                'preferredcodec': 'm4a',  # Cambiamos a m4a
                 'preferredquality': '192',
             }],
         }
 
         with yt_dlp.YoutubeDL(ydl_opts) as ydl:
             info = ydl.extract_info(url, download=True)
-            filename = f"{info['title']}.mp3"
+            filename = f"{info['title']}.m4a"  # Cambiar extensión a m4a
             file_path = os.path.join(DOWNLOAD_FOLDER, filename)
 
         # Enviar el archivo y eliminarlo después
-        response = send_file(file_path, as_attachment=True)
+        response = send_file(file_path, as_attachment=True, mimetype="audio/mp4")
         os.remove(file_path)  # Borra el archivo después de enviarlo
 
         return response
@@ -43,4 +43,3 @@ def download_audio():
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000)
-
